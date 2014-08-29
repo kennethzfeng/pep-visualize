27b586015cf06c80d82837578937e94217c77342
Kenneth Feng <kennethzfeng@gmail.com>
2014-08-29 02:35:36 -0400

    ISSUE#4: Refactored HTTP 400 Bad Request

--- app/__init__.py
+++ app/__init__.py
@@ -13,17 +13,21 @@ app = Flask(__name__)
 
 
 @app.errorhandler(404)
-def error(e):
+def error_404(e):
     return jsonify({'errors': ('Resource Not Found', )}), 404
+
+
+@app.errorhandler(400)
+def error_400(e):
+    return jsonify({'errors': ('Bad Request', )}), 400
 
 
 @app.route('/pep/<int:pep_number>')
 def get_pep(pep_number):
-    """
-    Get the content div of the PEP by PEP number
-    """
+    """Get the content div of the PEP by PEP number"""
     if not pep_number:
-        return jsonify(dict(errors=['PEP Number is missing'])), 400
+        return abort(400)
+
     with open('pep_documents/pep-%04d.txt' % pep_number, 'rb') as f:
         text = f.read().decode('utf-8')
     pep = PEP(text)

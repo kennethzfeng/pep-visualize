ea45ce2035148c6849a8493ea56474f07dcd1c05
Kenneth Feng <kennethzfeng@gmail.com>
2014-08-30 13:07:06 -0400

    ISSUE#5: Refactored parse_metadata into smaller functions

--- app/models.py
+++ app/models.py
@@ -24,23 +24,36 @@ class PEP(object):
                 if len(parts) == 2:
                     self.metadata_dict[parts[0]] = parts[1].strip()
 
+        self._parse_title()
+        self._parse_pep()
+        self._parse_authors()
+        self._parse_type()
+        self._parse_status()
+
+
+        self.pep_content = '\n\n\n'.join(self.pep_text.split('\n\n\n')[1:])
+
+    def _parse_title(self):
+        """Parse for Title"""
         if 'Title' in self.metadata_dict:
             self.title = self.metadata_dict['Title']
 
+    def _parse_pep(self):
         if 'PEP' in self.metadata_dict:
             self.number = self.metadata_dict['PEP']
 
+    def _parse_authors(self):
         if 'Author' in self.metadata_dict:
             author_string = self.metadata_dict['Author']
             self.authors = [x.strip() for x in author_string.split(',')]
 
+    def _parse_type(self):
         if 'Type' in self.metadata_dict:
             self.type = self.metadata_dict['Type']
 
+    def _parse_status(self):
         if 'Status' in self.metadata_dict:
             self.status = self.metadata_dict['Status']
-
-        self.pep_content = '\n\n\n'.join(self.pep_text.split('\n\n\n')[1:])
 
     def to_dict(self):
         return {

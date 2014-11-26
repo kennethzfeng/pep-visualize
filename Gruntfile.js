6fa477941bb0350a2ec085fcf158f7831f28a185
Kenneth Feng <kennethzfeng@gmail.com>
2014-11-25 23:43:49 -0500

    Feature #16: Use Grunt to setup Python Dev Env

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,15 +17,42 @@ module.exports = function(grunt) {
           layout: 'byComponent'
         }
       }
+    },
+
+    shell: {
+      pythonServer: {
+        options: {
+          stdout: true
+        },
+        command: 'env/bin/python run_dev_server.py'
+      },
+
+      virtualenv: {
+        options: {
+          stdout: true
+        },
+        command: 'virtualenv env'
+      },
+
+      pythonDeps: {
+        options: {
+          stdout: true
+        },
+        command: 'env/bin/pip install -r requirements.txt'
+      }
     }
 
   });
 
 
   grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('default', []);
   grunt.registerTask('install', ['bower']);
   grunt.registerTask('heroku:production', ['install']);
+  grunt.registerTask('dev:setup', ['shell:virtualenv', 'shell:pythonDeps']);
+  grunt.registerTask('dev:server', ['dev:setup', 'shell:pythonServer']);
+
 
 };
